828e98b9c0195092ce6d1d3a2ecc83ea139e8446
Sunit Parekh <parekh.sunit@gmail.com>
2016-04-18 15:13:41 +0530

    added support for pivotal cloud foundry.

--- src/main/java/sample/web/SampleController.java
+++ src/main/java/sample/web/SampleController.java
@@ -1,7 +1,9 @@
 package sample.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,10 +11,13 @@ import org.springframework.web.bind.annotation.*;
 @EnableAutoConfiguration
 public class SampleController {
 
+    @Autowired
+    private Environment env;
+
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World!";
+        return "Hello World! " + env.getProperty("CF_INSTANCE_GUID");
     }
 
     public static void main(String[] args) throws Exception {
